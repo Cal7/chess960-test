7ab91ee3a27461daf292899a12d3ef0131084844
Callum Browne <me+github@calbro.me>
2017-01-27 17:42:48 +0000

    Changed name of column

--- src/db_functions.py
+++ src/db_functions.py
@@ -4,7 +4,7 @@ from os.path import isfile
 def create_db():
     con = sqlite3.connect("../db/db.sqlite")
     cur = con.cursor()
-    cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `starting_id` INTEGER NOT NULL, `time_limit` INTEGER NOT NULL, `result` TEXT )")
+    cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `start_position_id` INTEGER NOT NULL, `time_limit` INTEGER NOT NULL, `result` TEXT )")
     cur.execute("CREATE TABLE `moves` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `number` INTEGER, `move` TEXT, `game_id` INTEGER )")
 
 def db_exists():

e701f4a39b6cb0e782999f6809edc77d33754417
Callum Browne <me+github@calbro.me>
2017-01-28 01:13:13 +0000

    Implemented saving to db

--- src/simulation.py
+++ src/simulation.py
@@ -10,11 +10,13 @@ class Simulation:
         self.info_handler = chess.uci.InfoHandler()
         self.engine.info_handlers.append(self.info_handler)
 
-        self.board = chess.Board.from_chess960_pos(start_position_id)
+        self.start_position_id = start_position_id
+        self.board = chess.Board.from_chess960_pos(self.start_position_id)
 
         #the number of milliseconds that each player is allotted at the start of the game
-        self.white_time = time_limit
-        self.black_time = time_limit
+        self.time_limit = time_limit
+        self.white_time = self.time_limit
+        self.black_time = self.time_limit
 
         #the number of milliseconds added to a player's remaining time after making a move
         self.time_increment = time_increment
@@ -42,3 +44,9 @@ class Simulation:
 
     def get_move_history(self): #returns a string representation of the move stack in standard uci form, i.e. "d2d4 d7d5 c2c4"
         return " ".join([str(move) for move in self.board.move_stack])
+
+    def save_to_db(self, con): #inserts this game into the sqlite database
+        cur = con.cursor()
+        cur.execute("INSERT INTO `games` (`start_position_id`, `time_limit`, `time_increment`, `result`, `move_history`) VALUES(?, ?, ?, ?, ?)",
+                    (self.start_position_id, self.time_limit, self.time_increment, self.board.result(True), self.get_move_history()))
+        con.commit()

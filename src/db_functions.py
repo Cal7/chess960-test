e4fedad112e8407311ec3098d33244f722096d38
Callum Browne <me+github@calbro.me>
2017-01-28 17:35:06 +0000

    Added get_db_path() function

--- src/db_functions.py
+++ src/db_functions.py
@@ -2,9 +2,12 @@ import sqlite3
 import os.path
 
 def create_db():
-    con = sqlite3.connect("../db/db.sqlite")
+    con = sqlite3.connect(get_db_path())
     cur = con.cursor()
     cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `start_position_id` INTEGER, `time_limit` INTEGER, `time_increment` INTEGER, `result` INTEGER, `move_history` TEXT )")
 
+def get_db_path():
+    return os.path.join(os.path.dirname(__file__), "../db/db.sqlite")
+
 def db_exists():
-    return os.path.isfile(os.path.join(os.path.dirname(__file__), "../db/db.sqlite"))
+    return os.path.isfile(get_db_path())

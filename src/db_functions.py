0b5f9f2142821bb677b5bd28f212c955f0efb924
Callum Browne <me+github@calbro.me>
2017-01-28 17:13:47 +0000

    db_exists contained a flaw where the path would be relative to the directory the script is invoked from, not the directory that db_functions.py is in

--- src/db_functions.py
+++ src/db_functions.py
@@ -1,5 +1,5 @@
 import sqlite3
-from os.path import isfile
+import os.path
 
 def create_db():
     con = sqlite3.connect("../db/db.sqlite")
@@ -7,4 +7,4 @@ def create_db():
     cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `start_position_id` INTEGER, `time_limit` INTEGER, `time_increment` INTEGER, `result` INTEGER, `move_history` TEXT )")
 
 def db_exists():
-    return isfile("../db/db.sqlite")
+    return os.path.isfile(os.path.join(os.path.dirname(__file__), "../db/db.sqlite"))

70a7ccaf8764d9b65d7d777a46c5e5178e1b177d
Callum Browne <me+github@calbro.me>
2017-01-28 01:11:48 +0000

    Fixed typo in column name

--- src/db_functions.py
+++ src/db_functions.py
@@ -4,7 +4,7 @@ from os.path import isfile
 def create_db():
     con = sqlite3.connect("../db/db.sqlite")
     cur = con.cursor()
-    cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `start_postion_id` INTEGER, `time_limit` INTEGER, `time_increment` INTEGER, `result` INTEGER, `move_history` TEXT )")
+    cur.execute("CREATE TABLE `games` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `start_position_id` INTEGER, `time_limit` INTEGER, `time_increment` INTEGER, `result` INTEGER, `move_history` TEXT )")
 
 def db_exists():
     return isfile("../db/db.sqlite")
